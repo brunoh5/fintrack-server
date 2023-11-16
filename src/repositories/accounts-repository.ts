c69c8a5317e99e6f3cc5d62b61e1b88302bf14a9
Bruno Henrique <brunohenriquesantos272@gmail.com>
2023-11-16 20:01:47 -0300

    feat: get account use case

--- src/repositories/accounts-repository.ts
+++ src/repositories/accounts-repository.ts
@@ -1,5 +1,6 @@
 import { Account, Prisma } from '@prisma/client'
 
 export interface AccountsRepository {
+	findById(id: string): Promise<Account | null>
 	create(data: Prisma.AccountUncheckedCreateInput): Promise<Account>
 }
---
+++ src/use-cases/get-account.ts
@@ -0,0 +1,36 @@
+import { AccountsRepository } from '@/repositories/accounts-repository'
+import { hideAccountNumber } from '@/utils/hide-account-number'
+import { Account } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetAccountUseCaseRequest {
+	accountId: string
+}
+
+interface GetAccountUseCaseResponse {
+	account: Account
+}
+
+export class GetAccountUseCase {
+	constructor(private accountsRepository: AccountsRepository) {}
+
+	async execute({
+		accountId,
+	}: GetAccountUseCaseRequest): Promise<GetAccountUseCaseResponse> {
+		const accountResponse = await this.accountsRepository.findById(accountId)
+
+		if (!accountResponse) {
+			throw new ResourceNotFoundError()
+		}
+
+		const account = Object.assign(accountResponse, {
+			number: accountResponse.number
+				? hideAccountNumber(accountResponse.number)
+				: null,
+		})
+
+		return {
+			account,
+		}
+	}
+}
