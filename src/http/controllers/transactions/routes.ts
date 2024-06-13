e21dadeb0d5a2e5a3d3009eae389fb248bf7ff46
Bruno Henrique <brunohenriquesantos272@gmail.com>
2024-06-13 13:03:44 -0300

    fix: removendo controller

--- src/http/controllers/transactions/routes.ts
+++ src/http/controllers/transactions/routes.ts
@@ -1,26 +1,18 @@
 import { Router } from 'express'
 
-// import multer from 'multer'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { create } from './create'
 import { deleteTransaction } from './delete'
 import { fetch } from './fetch'
 import { get } from './get'
-// import { Import } from './import'
 import { metrics } from './metrics'
 import { monthlyExpensesByYear } from './monthly-expenses-by-year'
 import { update } from './update'
 
-// const upload = multer({
-// 	dest: './tmp',
-// })
-
 const transactionsRouter = Router()
 
 transactionsRouter.use(verifyJWT)
-
-// const importTransactions = new Import()
 
 transactionsRouter.get('/transactions/monthly-expenses', monthlyExpensesByYear)
 transactionsRouter.get('/transactions/metrics', metrics)
@@ -30,11 +22,5 @@ transactionsRouter.post('/transactions', create)
 transactionsRouter.put('/transactions/:id', update)
 transactionsRouter.get('/transactions/:id', get)
 transactionsRouter.delete('/transactions/:id/delete', deleteTransaction)
-
-// transactionsRouter.post(
-// 	'/transactions/:accountId/import',
-// 	upload.single('file'),
-// 	importTransactions.handle,
-// )
 
 export { transactionsRouter }
